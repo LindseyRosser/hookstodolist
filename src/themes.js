ad25606f0234480d794a28f86e0e5959d87e4059
LindseyRosser <lindseyrosser8044@gmail.com>
2019-09-16 21:43:03 -0500

    Active, All, Complete functionality

--- src/themes.js
+++ src/themes.js
@@ -1,2 +1,33 @@
+import { createMuiTheme } from "@material-ui/core/styles";
 
+
+export const themeNight = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#564E58"
+    },
+
+    secondary: {
+      main: "#904E55"
+    },
+
+    type: "dark"
+  }
+});
+
+export const themeDay = createMuiTheme(
+    {
+        palette: {
+            primary: {
+              main: "#F2EFE9"
+            },
+        
+            secondary: {
+              main: "#EF8275"
+            },
+        
+            type: "light"
+          }
+    }
+);
 
